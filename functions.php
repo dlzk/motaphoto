a70c05a1b6fe9b5dcf5ccba7c66fd61e787284a6
Adil Zaki <zakiadil444@gmail.com>
2023-10-28 18:13:42 +0200

    Ajout des fichiers scripts.js et modal.php

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/styles.css' );
 }
+
