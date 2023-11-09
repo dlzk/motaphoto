4a16a27eab8ed59ae57b97fcb4df2b1dc75ecc67
Adil Zaki <zakiadil444@gmail.com>
2023-11-09 18:31:59 +0100

    Ajout du code responsive dans le modal

--- templates_part/modal.php
+++ templates_part/modal.php
@@ -1,7 +1,7 @@
 <div id="myModal" class="modal">
   <div class="modal-content">
     <div class="modal-header">
-      <img src="<?php echo get_stylesheet_directory_uri() . '/images/Contact-header.png' ?>" alt="Contact">
+      <!-- <img src="<?php echo get_stylesheet_directory_uri() . '/images/Contact-header.png' ?>" alt="Contact"> -->
     </div>
     <div class="modal-body">
       <?php echo do_shortcode( '[contact-form-7 id="a99227d" title="Contact form 1"]' ) ?>
